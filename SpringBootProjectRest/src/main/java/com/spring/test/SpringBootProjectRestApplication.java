12ba9865cdb609195fe2237ab9e868e871947434
AatulTomaR <AatulTomaR@lenovo.mshome.net>
2019-10-13 21:20:00 +0530

    first commit.

---
+++ SpringBootProjectRest/src/main/java/com/spring/test/SpringBootProjectRestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectRestApplication.class, args);
+	}
+
+}
